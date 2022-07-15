0fcf30bb4a1583892a738e706701c6feb4a35a0d
Chakpiwat Pacharajindawat <gameraroma@gmail.com>
2022-07-15 13:24:51 +0700

    show profit and unrealised price of assets

--- lib/models/gold_asset.dart
+++ lib/models/gold_asset.dart
@@ -2,15 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:gold_trading_playground/models/gold_prices.dart';
 import 'package:intl/intl.dart';
 
-enum GoldType {
-  bullion,
-  ornament
-}
+enum GoldType { bullion, ornament }
 
-enum GoldUnit {
-  baht,
-  quarterOfBaht
-}
+enum GoldUnit { baht, quarterOfBaht }
 
 class GoldAsset {
   final String id;
@@ -26,19 +20,21 @@ class GoldAsset {
   }
 
   String getUnrealisedDisplay(GoldPrices currentPrices) {
-    var currentPrice = type == GoldType.bullion ? currentPrices.blSell : currentPrices.omSell;
+    var currentPrice =
+        type == GoldType.bullion ? currentPrices.blBuy : currentPrices.omBuy;
     final bahtWeight = unit == GoldUnit.quarterOfBaht ? 0.25 * weight : weight;
     if (currentPrice == GoldPrices.naText) {
       return 'คำนวนไม่ได้';
     }
     currentPrice = currentPrice.replaceAll(',', '');
     var price = bahtWeight * double.parse(currentPrice);
-    var formatter = NumberFormat('#,###,000');
-    return '${formatter.format(price)} ฿';
+    var formatter = NumberFormat('#,000.00');
+    return formatter.format(price);
   }
 
   String getProfitDisplay(GoldPrices currentPrices) {
-    var currentPrice = type == GoldType.bullion ? currentPrices.blSell : currentPrices.omSell;
+    var currentPrice =
+        type == GoldType.bullion ? currentPrices.blBuy : currentPrices.omBuy;
     final bahtWeight = unit == GoldUnit.quarterOfBaht ? 0.25 * weight : weight;
     if (currentPrice == GoldPrices.naText) {
       return 'คำนวนไม่ได้';
@@ -46,12 +42,25 @@ class GoldAsset {
     currentPrice = currentPrice.replaceAll(',', '');
     var price = bahtWeight * double.parse(currentPrice);
     var profit = price - cost;
-    var formatter = NumberFormat('#,###,000');
-    return '${formatter.format(profit)} ฿';
+    var formatter = NumberFormat('#,000.00');
+    var sign = profit > 0 ? '+' : '';
+    return '$sign${formatter.format(profit)}';
+  }
+
+  Color getProfitTextColor(GoldPrices currentPrices) {
+    final profit = getProfit(currentPrices);
+    if (profit > 0) {
+      return Colors.green;
+    } else if (profit < 0) {
+      return Colors.red;
+    } else {
+      return Colors.black;
+    }
   }
 
   double getProfit(GoldPrices currentPrices) {
-    var currentPrice = type == GoldType.bullion ? currentPrices.blSell : currentPrices.omSell;
+    var currentPrice =
+        type == GoldType.bullion ? currentPrices.blSell : currentPrices.omSell;
     final bahtWeight = unit == GoldUnit.quarterOfBaht ? 0.25 * weight : weight;
     if (currentPrice == GoldPrices.naText) {
       return 0;
@@ -64,7 +73,8 @@ class GoldAsset {
 
   GoldAsset(this.id, this.name, this.type, this.cost, this.weight, this.unit);
 
-  factory GoldAsset.createNew(String name, GoldType type, double cost, int weight, GoldUnit unit) {
+  factory GoldAsset.createNew(
+      String name, GoldType type, double cost, int weight, GoldUnit unit) {
     return GoldAsset(UniqueKey().toString(), name, type, cost, weight, unit);
   }
 }
--- lib/views/assets_page.dart
+++ lib/views/assets_page.dart
@@ -16,7 +16,7 @@ class _AssetsPageState extends ConsumerState<AssetsPage> {
         "ทองแท่งร้านฮั่วเซ่งเฮง", GoldType.bullion, 160000, 5, GoldUnit.baht),
     GoldAsset.createNew("ลายมังกร", GoldType.ornament, 81000, 3, GoldUnit.baht),
     GoldAsset.createNew(
-        "แหวนเกลี้ยง", GoldType.ornament, 8500, 2, GoldUnit.quarterOfBaht),
+        "แหวนเกลี้ยง", GoldType.ornament, 14000, 2, GoldUnit.quarterOfBaht),
   ];
 
   @override
@@ -35,30 +35,63 @@ class _AssetsPageState extends ConsumerState<AssetsPage> {
                 ? Icons.all_inclusive
                 : Icons.crop_portrait;
             String unit = asset.unit == GoldUnit.baht ? 'บาท' : 'สลึง';
-            return ListTile(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 0.0, horizontal: 4.0),
-                minLeadingWidth: 12,
-                leading: Icon(typeIcon),
-                title: Text(asset.name),
-                subtitle: Text('${asset.weight} $unit'),
-                trailing: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
                   children: [
-                    Text(
-                      asset.getUnrealisedDisplay(goldPrices),
-                      style: const TextStyle(fontSize: 20),
+                    Expanded(
+                      flex: 1,
+                      child: Icon(typeIcon),
                     ),
-                    const SizedBox(
-                      width: 0,
-                      height: 8,
+                    Expanded(
+                      flex: 5,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 12),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(asset.name,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context).textTheme.titleMedium),
+                            Text(
+                              '${asset.weight} $unit',
+                              style: Theme.of(context).textTheme.titleSmall,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    Text(
-                      '(${asset.getProfitDisplay(goldPrices)})',
-                      style: const TextStyle(fontSize: 16),
+                    Expanded(
+                      flex: 4,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            asset.getUnrealisedDisplay(goldPrices),
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                          const SizedBox(
+                            width: 0,
+                            height: 8,
+                          ),
+                          Text(
+                            asset.getProfitDisplay(goldPrices),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: asset.getProfitTextColor(goldPrices),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
-                ));
+                ),
+              ),
+            );
           }),
       error: (err, stack) => const Text('ไม่สามารถดึงข้อมูลได้'),
       loading: () => const Center(child: CircularProgressIndicator()),
